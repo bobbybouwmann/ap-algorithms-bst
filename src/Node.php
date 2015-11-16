a31164418f71c47bcfd8f7d153a975e49eb064d3
Bobby Bouwmann <bobbybouwmann@gmail.com>
2015-11-16 19:30:47 +0100

    Refactor some stuff

--- src/Node.php
+++ src/Node.php
@@ -2,104 +2,155 @@
 
 namespace App;
 
-class Node {
+class Node
+{
+    /**
+     * The level of the node.
+     *
+     * @var int
+     */
+    public $level = 1;
 
-	public $level = 1;
-	public $data = null;
-	public $left = null;
-	public $right = null;
+    /**
+     * The data of the node.
+     *
+     * @var null
+     */
+    public $data = null;
 
-	public function __construct($data = null)
-	{
-		$this->data = $data;
-	}
+    /**
+     * The left child node.
+     *
+     * @var null
+     */
+    public $left = null;
 
-	public function insert($node) {
-		if ($node->data < $this->data) {
-			$node->level++;
-			if ($this->left !== null) {
-				$this->left->insert($node);
-			} else {
-				$this->left = $node;
-			}
-		} else {
-			if ($this->right !== null) {
-				$this->right->insert($node);
-			} else {
-				$this->right = $node;
-			}
-		}
-	}
+    /**
+     * The right child node.
+     *
+     * @var null
+     */
+    public $right = null;
 
-	public function delete($data) {
-		if ($data < $this->data) { // element is in the left tree
-			if ($this->left !== null) {
-				$res = $this->left->delete($data); // delete from left tree
+    /**
+     * Create the Node with the given data.
+     *
+     * @param null $data
+     */
+    public function __construct($data = null)
+    {
+        $this->data = $data;
+    }
 
-				if ($res && $this->left->right === null && $this->left->left === null) {
-					$this->left = null;
-				}
-				return false;
-			} else {
-				return false; // element is not in the tree
-			}
-		} else { // element is either this node or in our right tree
+    /**
+     * Insert a new node.
+     *
+     * @param $node
+     */
+    public function insert($node)
+    {
+        if ($node->data < $this->data) {
+            $node->level++;
 
-			if ($this->data === $data) { // we have found our node to delete.
+            if ($this->left !== null) {
+                $this->left->insert($node);
+            } else {
+                $this->left = $node;
+            }
+        } else {
+            if ($this->right !== null) {
+                $this->right->insert($node);
+            } else {
+                $this->right = $node;
+            }
+        }
+    }
 
-				if ($this->right !== null) {
+    /**
+     * Delete a node based on the given data.
+     *
+     * @param $data
+     *
+     * @return bool
+     */
+    public function delete($data)
+    {
+        // element is in the left tree
+        if ($data < $this->data) {
+            if ($this->left !== null) {
+                // delete from left tree
+                $res = $this->left->delete($data);
 
-					$rightElement = $this->right;
+                if ($res && $this->left->right === null && $this->left->left === null) {
+                    $this->left = null;
+                }
+            }
 
-					$this->data = $rightElement->data; // replace our data
-					$tempLeft = $this->left; // keep our left value
+            return false;
+        } else { // element is either this node or in our right tree
+            if ($this->data === $data) { // we have found our node to delete.
+                if ($this->right !== null) {
+                    $rightElement = $this->right;
 
-					// Move our right element childs to our left and right
-					$this->left = $rightElement->left;
-					$this->right = $rightElement->right;
+                    // replace our data
+                    $this->data = $rightElement->data;
 
-					if ($tempLeft !== null) {
-						// Place the left child in our new tree
-						$this->insert($tempLeft);
-					}
-					return true;
-				} elseif ($this->left !== null) {
-					// We know that the right element is not present. So we just replace our current data by left
+                    // keep our left value
+                    $tempLeft = $this->left;
 
-					$elementToMoveUp = $this->left;
-					$this->data = $elementToMoveUp->data; // replace our data
+                    // Move our right element children to our left and right
+                    $this->left = $rightElement->left;
+                    $this->right = $rightElement->right;
 
-					// Move our left element childs to our left and right
-					$this->left = $elementToMoveUp->left;
-					$this->right = $elementToMoveUp->right;
-				} else {
-					// Both childs are null
-					return true;
-				}
-			} else {
-				if ($this->right !== null) {
-					$res = $this->right->delete($data); // delete from right tree
-					if ($res && $this->right->right === null && $this->right->left === null) {
-						$this->right = null;
-					}
-					return false;
+                    if ($tempLeft !== null) {
+                        // Place the left child in our new tree
+                        $this->insert($tempLeft);
+                    }
+                } elseif ($this->left !== null) {
+                    // We know that the right element is not present. So we just replace our current data by left
+                    $elementToMoveUp = $this->left;
 
-				} else {
-					return false; // element is not in the tree
-				}
-			}
-		}
-	}
+                    // replace our data
+                    $this->data = $elementToMoveUp->data;
 
-	public function search($data) {
-		if ($data < $this->data) { // element is in the left tree
-			return $this->left !== null ? $this->left->search($data) : null;
-		} else { // element is either this node or in our right tree
-			if ($this->data === $data) { // we have found our node.
-				return $this;
-			} else {
-				return $this->right !== null ? $this->right->search($data) : null;
-			}
-		}
-	}
+                    // Move our left element children to our left and right
+                    $this->left = $elementToMoveUp->left;
+                    $this->right = $elementToMoveUp->right;
+                } else {
+                    return true;
+                }
+            } else {
+                if ($this->right !== null) {
+                    // delete from right tree
+                    $res = $this->right->delete($data);
+
+                    if ($res && $this->right->right === null && $this->right->left === null) {
+                        $this->right = null;
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Search through the tree for a certain Node with the correct data.
+     *
+     * @param $data
+     *
+     * @return $this|null
+     */
+    public function search($data)
+    {
+        if ($data < $this->data) { // element is in the left tree
+            return $this->left !== null ? $this->left->search($data) : null;
+        } else { // element is either this node or in our right tree
+            if ($this->data === $data) { // we have found our node.
+                return $this;
+            } else {
+                return $this->right !== null ? $this->right->search($data) : null;
+            }
+        }
+    }
 }
